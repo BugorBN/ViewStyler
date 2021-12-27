8956b4a8ec86ec1ff763a8512576a505a57ed928
Boris Bugor <b.bugor@headsandhands.ru>
2021-12-27 17:51:14 +0300

    updated kingfisher

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher", from: "6.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.1.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
